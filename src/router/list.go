a0b9e8360e7947422d6aadd253e07ad82df78586
Oleg Tsyparynda <o.tsiparinda@gmail.com>
2026-06-23 16:40:10 +0200

    edit only

--- src/router/list.go
+++ src/router/list.go
@@ -8,30 +8,14 @@ import (
 
 func ListRouters(routers []models.Router) error {
 
-	fmt.Printf(
-		"%-5s %-25s %-15s %-6s %-8s %-8s\n",
-		"ID",
-		"NAME",
-		"IP",
-		"MAIN",
-		"PARENT",
-		"ROS",
-	)
+	fmt.Printf("%-5s %-25s %-15s %-6s %-8s %-8s\n", "ID", "NAME", "IP", "MAIN", "PARENT", "ROS")
 	sort.Slice(routers, func(i, j int) bool {
 		return routers[i].Name < routers[j].Name
 	})
 
 	for _, r := range routers {
 
-		fmt.Printf(
-			"%-5d %-25s %-15s %-6t %-5d %-6s \n",
-			r.ID,
-			r.Name,
-			r.IP,
-			r.Main,
-			r.ParentID,
-			r.ROSVersion,
-		)
+		fmt.Printf("%-5d %-25s %-15s %-6t %-5d %-6s \n", r.ID, r.Name, r.IP, r.Main, r.ParentID, r.ROSVersion)
 	}
 
 	fmt.Printf("\nRouters: %d\n", len(routers))
